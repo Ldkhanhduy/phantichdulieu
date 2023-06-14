e53cfdfc55996fec885eb06b7eaf15c16fcfb669
Ldkhanhduy <90101790+Ldkhanhduy@users.noreply.github.com>
2023-06-14 14:00:59 +0700

    Update uocluong.py

--- uocluong.py
+++ uocluong.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 #Bài toán ước lượng tỉ lệ xe c bán được từ trước nay đến 2022
 mau = pd.read_excel("D:/Duy/honda_sample.xlsx", index_col=None)
-a = pd.crosstab(index=mau["Make"], columns=mau["Condition"])
+a = pd.crosstab(index=mau["Make"], columns=mau["Condition"]) #bang tan so 
 #Số lượng mẫu
 n = mau.shape[0]
 print("Số lượng mẫu:", n)
@@ -14,5 +14,5 @@ print("Số lượng xe cũ:", m)
 f = m/n
 print("Tỉ lệ mẫu:", f)
 #Với độ tin cậy 95%, khoảng ước lượng tỉ lệ là
-khoang = prop.proportion_confint(m, n, alpha=0.05)
+khoang = prop.proportion_confint(m, n, alpha=0.05) #ham tinh khoang uoc luong ti le
 print("Với độ tin cậy là 95%, khoảng ước lượng tỉ lệ bán xe cũ là:", khoang)
